66d173d4a30901182cc99b3e56464e21d0e18135
mhmmdd <mhmmd.dd@gmail.com>
2016-07-03 16:38:27 +0300

    Fixes #2

--- dev/component2.component.ts
+++ dev/component2.component.ts
@@ -1,10 +1,16 @@
 import {Component} from "angular2/core";
+import {Router} from "angular2/router";
 
 @Component({
   template: `
     <h1>Component 2</h1>
+    <button (click)="onNavigate()">Take me to Component 1</button>
   `
 })
 export class Component2Component {
+  constructor(private _router: Router) {}
 
+  onNavigate() {
+    this._router.navigate(['Component1']);
+  }
 }
